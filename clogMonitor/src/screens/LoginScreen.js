35f96aec407d7fdaa8f42c8688dc4d1434815cf0
EvanGao0815 <34082772+EvanGao0815@users.noreply.github.com>
2022-03-10 11:59:11 -0500

    Made mockup of Login Page
    Functionality not currently implemented

--- clogMonitor/src/screens/LoginScreen.js
+++ clogMonitor/src/screens/LoginScreen.js
@@ -1,7 +1,45 @@
-import React from "react";
+import logo from '../components/logo.svg'; //change directory
+import '../screens/LoginScreen.css'
+
+
+const database = [
+  {
+    username: "Mark",
+    password: "123"
+  }
+];
 
 function LoginScreen() {
-  return <div>LoginScreen</div>;
+  return (
+    <div>
+      <form className="LoginScreen">
+        <h1>
+          Clog Monitor Login
+        </h1>
+        <form>
+          <label>
+            Username:
+            <input type="text" name="name" required/>
+          </label>
+          <label>
+            <br/>
+            Password:
+            <input type="text" name="pass" required/>
+          </label>
+        </form>
+        <label>
+          Remember Password?
+          <input type="checkbox" name="remember" />
+        </label>
+        <label>
+          Forgot Password?
+          <input type="submit" name="Get Help" />
+        </label>
+        <br/>
+        <input type="submit" value="Login" />
+        </form>
+      </div>
+  );
 }
 
 export default LoginScreen;
